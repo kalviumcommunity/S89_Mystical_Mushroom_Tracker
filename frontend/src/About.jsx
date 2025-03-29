df142944fc234f6efdaced32ffd97247a8bbe915
chitti239 <sharinis239@gmail.com>
2025-03-29 11:06:03 +0530

    added home page

---
+++ frontend/src/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div style={{ textAlign: "center", fontFamily: "Arial"}}>
+      <h1>🌿 Mystical Mushroom Tracker 🍄</h1>
+      <p>Discover, track, and manage your collection of mystical mushrooms.</p>
+      <p>The Mystical Mushroom Tracker app lets you discover, document, and manage your mushroom collection effortlessly. Ideal for explorers and mycology enthusiasts, it helps you track details like location and properties, creating a personalized database. Dive into the magical world of fungi and make every adventure unforgettable. 🍄✨</p>
+      <button style={{
+          padding: "10px 20px",
+          backgroundColor: "green",
+          color: "white",
+          border: "none",
+          borderRadius: "5px",
+          cursor: "pointer",
+      }}>
+        Get Started
+      </button>
+    </div>
+
+  )
+}
+
+export default About
